9ed1db4b5552024b27ddfd39dcf6c0cf2aef0d52
theocesar <theocesar@live.com>
2023-02-19 10:54:05 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,7 @@
 """
 // Théo César Zanotto da Silva
 
-ENUNCIADO
-
-Sua tarefa será  gerar a matriz termo documento, dos documentos recuperados da internet e
+O algoritmo deve gerar a matriz termo documento, dos documentos recuperados da internet e
 imprimir esta matriz na tela. Para tanto:
 
 a) Considere que todas as listas de sentenças devem ser transformadas em listas de vetores,
@@ -27,6 +25,8 @@ import pandas as pd
 array1 = []
 a1 = []
 matrix = []
+
+# chosen urls
 urls = ('https://hbr.org/2022/04/the-power-of-natural-language-processing',
         'https://monkeylearn.com/natural-language-processing/',
         'https://www.sas.com/en_us/insights/analytics/what-is-natural-language-processing-nlp.html',
@@ -34,14 +34,18 @@ urls = ('https://hbr.org/2022/04/the-power-of-natural-language-processing',
         'https://en.wikipedia.org/wiki/Natural_language_processing')
 
 for url in urls:
+    # obtaining all the content of the page
     html_page = urlopen(url).read()
     soup = BeautifulSoup(html_page, 'html.parser')
 
+    # removing all the unnecessary tags
     for uw in soup(['style', 'script', 'head', 'header', 'meta', '[document]', 'title', 'footer', 'iframe', 'nav']):
         uw.decompose()
 
+    # compacting the data
     end_r = ' '.join(soup.stripped_strings)
 
+    # splitting the sentences in single words and appending it to the array.
     for j in end_r.split():
         sen = str(j)
         a = sen.split(" ")
@@ -51,6 +55,7 @@ for url in urls:
             else:
                 continue
 
+    # counting the frequency of each word in the array.
     for sent in end_r.split():
         pav = str(sent).split()
         times = [0] * len(array1)
@@ -61,6 +66,7 @@ for url in urls:
 
         matrix.append(times)
 
-df = pd.DataFrame(matrix[:100], columns = array1[:919])
+# displaying the result using a pandas dataframe
+df = pd.DataFrame(matrix[:100], columns = array1[:920])
 df
 
